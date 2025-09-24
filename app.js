6eea56ab105ad1aab7506049e4f3f1251b43116f
“bonifaci0cleargit config --global user.name “bonifaci0 <andres.bonifaciob@gmail.com>
2025-09-23 21:17:34 -0400

    Implementar la lógica para agregar amigos y actualizar la lista en app.js

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ const botonAgregar = document.getElementById('button-add');
 const listaAmigos = document.getElementById('listaAmigos');
 
 function agregarAmigo(){ 
-    
+    const nombre = ingresarNombre.value;
+    amigos.push(nombre);
+    console.log(amigos);
+    ingresarNombre.value = '';
+    actualizarLista();
+}
+
+function actualizarLista(){
+    listaAmigos.textContext = amigos.join(', ');
 }
 
 function sortearAmigo(){
